67b65b8a990e517946011c823304cbbddf3264fa
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2018-10-08 12:33:34 +0200

    error handling

--- js/site.js
+++ js/site.js
@@ -1,17 +1,23 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
+/* install service worker */
+if ('serviceWorker' in navigator && location.protocol != "file:") {
+    navigator.serviceWorker.register('service-worker.js')
 }
 
-/* highlight current day */
-var els = document.querySelectorAll("a[href='http://domain.com']");
 
 /* input time */
 element = document.getElementById("time")
 strftime = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: "2-digit", minute: "2-digit", timeZoneName: "short"};
 time = new Intl.DateTimeFormat("de",strftime).format()
-element.textContent = time
+if(element){
+    element.textContent = time
+}
 
 /* highlight current day */
 var date = new Date()
-element = document.getElementById("day-"+date.getDate())
-element.style.background = "orange"
+if(window.location.href.includes("&"+(date.getMonth()+1).toString())){
+    element = document.getElementById("day-"+date.getDate())
+    if(element){
+    
+      element.style.background = "orange"
+    }
+}
