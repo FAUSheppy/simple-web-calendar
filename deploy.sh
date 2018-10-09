474f61d7ce6d4d3e9da8aa86880429e7e1c3376e
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2018-10-09 09:35:39 +0200

    sophisticate deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,23 @@ DEPLOY=${2}
 
 while /bin/true
 do
+
+    # output date for debug
     echo -n "Reubuild at: "
     date
+
+    # run the build
     ./python/cal.py $ICS $DEPLOY css js
+
+    # copy css/js
+    cp -f -r css/ ${DEPLOY}/css
+    cp -f -r css/ ${DEPLOY}/css
+    
+    # fix permissions to be sure
+    chown -R :www-data $DEPLOY
+    chmod -R g+rX $DEPLOY
+
+    # wait for changes
     inotifywait -e modify $ICS
+
 done
-chmod -R a+rX $DEPLOY
