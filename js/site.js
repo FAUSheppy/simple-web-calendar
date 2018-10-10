96a35b151eddb55934aeb9abf5825462aec99fa2
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2018-10-10 18:45:55 +0200

    fix path

--- js/site.js
+++ js/site.js
@@ -31,7 +31,7 @@ function runShit(){
     
     /* install service worker */
     if ('serviceWorker' in navigator && location.protocol != "file:") {
-        navigator.serviceWorker.register('worker.js')
+        navigator.serviceWorker.register('js/worker.js')
     }
     
     
