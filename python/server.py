3face163c0fc64ee144802fc4db2fdd4b35c7177
Yannik Schmidt <sheppy@atlantishq.de>
2019-06-27 21:22:22 +0200

    [git fast commit] 27. Jun 2019 - 21:22:22

--- python/server.py
+++ python/server.py
@@ -1,12 +1,28 @@
 import flask
+import sys
+
+import backends.filesystem
+import utils.timeframe
+
+backend = backends.filesystem
 
 @app.route("/monthview")
 def monthView():
-    return  ""
+    month = flask.request.get("month")
+    year  = flask.request.get("year")
+
+    events = backend.getEventTimestampsTupel()
+    utils.timeframe.
+
+    return flask.render_template()
 
 @app.route("/dayview")
 def dayView():
-    return ""
+    day   = flask.request.get("day")
+    month = flask.request.get("month")
+    year  = flask.request.get("year")
+
+    return flask.render_template()
 
 @app.route("/eventview")
 def eventView():
@@ -17,5 +33,34 @@ def sendStatic(path):
     return flask.send_from_directory('static', path)
 
 if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='Start open-leaderboard',
+                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # general parameters #
+    parser.add_argument("-i", "--interface", default="0.0.0.0", help="Interface to listen on")
+    parser.add_argument("-p", "--port",      default="5000", help="Port to listen on")
+    parser.add_argument("-b", "--backend",   default="filesystem", help="Backend to use")
+
+    # backend specific parameters #
+    parser.add_argument("--auth-file", default="auth.token", \
+                            help="Auth file for backend if nessesary")
+    parser.add_argument("--remote-url", default=None, \
+                            help="Remote url when using backend remote or google")
+    parser.add_argument("--fs-backend-path", default="data", \
+                            help="Path for locale file if backend 'filesystem' is used")
+    
+    #  set backend #
+    if args.backend == "fileystem":
+        backend = backends.fileystem
+    else:
+        print("Unsupportet backend", file=sys.stderr)
+        sys.exit(1)
+
+    #  set backend variables #
+
+
+    # startup #
+    args = parser.parse_args()
     app = flask.Flask("epic-open-calendar-frontend")
-    app.run()
+    app.run(interface=args.interface, port=args.port)
