27cb4dfb2ec108ec072e009be88ce7f1828101f8
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2020-02-03 21:10:16 +0100

    improve event create dropdown

--- static/js/eventCreate.js
+++ static/js/eventCreate.js
@@ -1,9 +1,23 @@
-<script>
-    function cancle(){
-    	 document.getElementById("event-form").reset()
-    }
+document.getElementById('start-date-picker').valueAsDate = new Date();
+document.getElementById('end-date-picker').valueAsDate   = new Date();
 
-    function submit(){
-        document.getElementById("event-form").submit()
-    }
-</script>
+function cancle(){
+	document.getElementById("event-form").reset()
+	document.getElementById("eventcreate-dropdown-container").style.display = 'none'
+	document.getElementById("eventcreate-dropdown-button").style.opacity 	= 1
+}
+
+function submit(){
+	document.getElementById("event-form").submit()
+}
+
+document.getElementById("eventcreate-dropdown-button").addEventListener('click', function(e){
+	el = document.getElementById("eventcreate-dropdown-container")
+	if(el.style.display == 'block'){
+		el.style.display = 'none'
+		document.getElementById("eventcreate-dropdown-button").style.opacity = 1
+	}else{
+		el.style.display = 'block'
+		document.getElementById("eventcreate-dropdown-button").style.opacity = 0.6
+	}
+});
