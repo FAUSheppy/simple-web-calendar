dbc2386b233161a56d5ebfb3b55c290ff9674613
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2020-02-03 20:00:00 +0100

    work on dropdown event create

---
+++ static/js/eventCreate.js
@@ -0,0 +1,9 @@
+<script>
+    function cancle(){
+    	 document.getElementById("event-form").reset()
+    }
+
+    function submit(){
+        document.getElementById("event-form").submit()
+    }
+</script>
