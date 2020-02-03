e25acf1bc5597c7a5dbf0bf57a275fde21bb7711
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2020-02-03 23:51:28 +0100

    visual improvements to dropdown

--- static/js/eventCreate.js
+++ static/js/eventCreate.js
@@ -3,7 +3,8 @@ document.getElementById('end-date-picker').valueAsDate   = new Date();
 
 function cancle(){
 	document.getElementById("event-form").reset()
-	document.getElementById("eventcreate-dropdown-container").style.display = 'none'
+	document.getElementById("eventcreate-dropdown-container").style.visibility = 'hidden'
+	document.getElementById("eventcreate-dropdown-container").style.opacity = 0
 	document.getElementById("eventcreate-dropdown-button").style.opacity 	= 1
 }
 
@@ -13,11 +14,13 @@ function submit(){
 
 document.getElementById("eventcreate-dropdown-button").addEventListener('click', function(e){
 	el = document.getElementById("eventcreate-dropdown-container")
-	if(el.style.display == 'block'){
-		el.style.display = 'none'
+	if(el.style.visibility == 'visible'){
+		el.style.opacity = 0
+		el.style.visibility = 'hidden'
 		document.getElementById("eventcreate-dropdown-button").style.opacity = 1
 	}else{
-		el.style.display = 'block'
+		el.style.visibility = 'visible'
+		el.style.opacity = 1
 		document.getElementById("eventcreate-dropdown-button").style.opacity = 0.6
 	}
 });
