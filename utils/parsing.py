bd59afa9bc4914f416a5513a84262e2e89b49232
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2020-02-07 20:30:26 +0100

    remove debug message

--- utils/parsing.py
+++ utils/parsing.py
@@ -65,7 +65,6 @@ def mapLinkFromLocation(location):
     baseUrl = "https://www.google.com/maps/dir/?api=1&destination={}"
     locationAdditive = location.replace(" ","+")
     link = baseUrl.format(urllib.parse.quote(locationAdditive, safe="+"))
-    print(link)
     return link
 
 def parseEventData(eventData):
